272a17cf8264f1c02b2dcaab1f372c0d02b96944
kaylakim368 <86245651+kaylakim368@users.noreply.github.com>
2022-07-10 22:38:02 +0700

    Add files via upload

---
+++ JavaBasic/Strings.java
@@ -0,0 +1,20 @@
+public class Strings {
+    public static void main(String[] args) {
+        int myInt = 7;
+
+        String text = "Hello";
+
+        String blank = " ";
+
+        String name = "Kayla";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+        System.out.println("Hello" + " " + "Do");
+        System.out.println("My integer is: " + myInt);
+
+        double myDouble = 7.8;
+        System.out.println("My double is: " + myDouble + ".");
+    }
+}
