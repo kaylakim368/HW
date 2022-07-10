2e792caa26ccbb208118d3c4099a8747dd405b71
kaylakim368 <86245651+kaylakim368@users.noreply.github.com>
2022-07-10 20:46:24 +0700

    Add files via upload

---
+++ HW.java
@@ -0,0 +1,22 @@
+public class HW {
+    public static void main(String[] args) {
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 9797;
+        double myDouble = 7.7237;
+        float myFloat = 343.3f;
+        char myChar = 'y';
+        boolean myBoolean = false;
+        byte myByte = 127;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+        // System.out.println("Hello world!");
+    }
+}
